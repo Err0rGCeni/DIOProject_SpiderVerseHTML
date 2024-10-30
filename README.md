f6b88c13f0fd521cf85b72a9ae711b60ddb026e0
GCeni <gceni10@gmail.com>
2024-10-30 10:44:02 -0300

    Alterações Finalizadas

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Live Spider-Man Multiversos
+
+![Print Home](./doc/home.png)
+
+Projeto (fork) de uma página HTML exibindo as três versões _live-action_ e suas atuais filmografias (2024).
+
+## Alterações
+
+- [ ] Adição de `alt` faltantes;
+- [ ] Refatoração do script para `selectCarouselItem()` e separação de `s-cards-carousel` para o arquivo `.css`;
+- [ ] Redirecionamento aplicado aos `.s-menu__item`;
+- [ ] Refatoramentos de acessibilidade;
+- [ ] Refatoramento das páginas com Fancybox Script;
+![fancybox](./doc/fancybox.png)
+- [ ] Refatoramento de layout das páginas dos atores;
+![Print Telas](./doc/telas.png)
+- [ ] Implementação de `.css` e `<script>` de scrolls temáticos;
+![Scrolls](./doc/scrolls.gif)
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -23,12 +23,9 @@ document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
 function selectCarouselItem(selectedButtonElement) {
   const selectedItem = selectedButtonElement.id;
   const carousel = document.querySelector('.s-cards-carousel');
-  const transform = carousel.style.transform;
-  const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
   const rotateYDeg = -120 * (Number(selectedItem) - 1);
-  const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
 
-  carousel.style.transform = newTransform;
+  carousel.style.transform = `translateZ(-40vw) rotateY(${rotateYDeg}deg)`;
 
   const activeButtonElement = document.querySelector('.s-controller__button--active');
   activeButtonElement.classList.remove('s-controller__button--active');
